3f4999b0a39ef9fa45954b29e56f34bd630a07ad
Andy Lu <qingjielu990916@gmail.com>
2025-11-29 15:10:11 -0500

    add cross pile image

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,25 +1,25 @@
 go build main.go
 ./main 500 1
 ./main 500 2
-# ./main 500 5
-# ./main 500 10
-# ./main 500 15
-# ./main 500 20
-# ./main 1000 1
-# ./main 1000 2
-# ./main 1000 5
-# ./main 1000 10
-# ./main 1000 15
-# ./main 1000 20
-# ./main 5000 1
-# ./main 5000 2
-# ./main 5000 5
-# ./main 5000 10
-# ./main 5000 15
-# ./main 5000 20
-# ./main 10000 1
-# ./main 10000 2
-# ./main 10000 5
-# ./main 10000 10
-# ./main 10000 15
-# ./main 10000 20
+./main 500 5
+./main 500 10
+./main 500 15
+./main 500 20
+./main 1000 1
+./main 1000 2
+./main 1000 5
+./main 1000 10
+./main 1000 15
+./main 1000 20
+./main 5000 1
+./main 5000 2
+./main 5000 5
+./main 5000 10
+./main 5000 15
+./main 5000 20
+./main 10000 1
+./main 10000 2
+./main 10000 5
+./main 10000 10
+./main 10000 15
+./main 10000 20
