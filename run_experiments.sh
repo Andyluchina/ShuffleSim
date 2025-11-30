ec20c462f352eaffe5a7c177bcd4a831f35737d5
Andy Lu <qingjielu990916@gmail.com>
2025-11-30 18:45:52 -0500

    extend to 10k

--- run_experiments.sh
+++ run_experiments.sh
@@ -5,39 +5,33 @@
 ./main 200 10
 ./main 200 15
 ./main 200 20
-./main 400 1
-./main 400 2
-./main 400 5
-./main 400 10
-./main 400 15
-./main 400 20
-./main 800 1
-./main 800 2
-./main 800 5
-./main 800 10
-./main 800 15
-./main 800 20
+./main 500 1
+./main 500 2
+./main 500 5
+./main 500 10
+./main 500 15
+./main 500 20
 ./main 1000 1
 ./main 1000 2
 ./main 1000 5
 ./main 1000 10
 ./main 1000 15
 ./main 1000 20
-# ./main 2000 1
-# ./main 2000 2
-# ./main 2000 5
-# ./main 2000 10
-# ./main 2000 15
-# ./main 2000 20
-# ./main 5000 1
-# ./main 5000 2
-# ./main 5000 5
-# ./main 5000 10
-# ./main 5000 15
-# ./main 5000 20
-# ./main 10000 1
-# ./main 10000 2
-# ./main 10000 5
-# ./main 10000 10
-# ./main 10000 15
-# ./main 10000 20
+./main 2000 1
+./main 2000 2
+./main 2000 5
+./main 2000 10
+./main 2000 15
+./main 2000 20
+./main 5000 1
+./main 5000 2
+./main 5000 5
+./main 5000 10
+./main 5000 15
+./main 5000 20
+./main 10000 1
+./main 10000 2
+./main 10000 5
+./main 10000 10
+./main 10000 15
+./main 10000 20
